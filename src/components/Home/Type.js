fb66f14e43a654ce2b0c0a85e253781a504b0b3c
AZEEMAZAM1 <MUHAMMADAZEEM3010@GMAIL.COM>
2025-05-11 22:22:36 +0100

    Update: added new features

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -6,10 +6,10 @@ function Type() {
     <Typewriter
       options={{
         strings: [
-          "Software Developer",
-          "Freelancer",
-          "MERN Stack Developer",
-          "Open Source Contributor",
+          "ACCOUNTANT ",
+          "FINANCIAL ANALYST",
+          "TAX CONSULTANT",
+          "AUDITOR",
         ],
         autoStart: true,
         loop: true,
