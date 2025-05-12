cb35ec8bab59f79d9786225362fe8fb39f910c28
AZEEMAZAM1 <MUHAMMADAZEEM3010@GMAIL.COM>
2025-05-12 03:40:36 +0100

    Your commit message

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,16 +8,16 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">USAMA AZAM </span>
+            Hi Everyone, I am <span className="purple">MUAZAM AZAM </span>
             from <span className="purple"> DUBAI, UAE.</span>
             <br />
-            I am currently employed as a Accountant at pixler.
+            I am currently employed as a senior medical billing assistant.
             <br />
-            I have completed ACCA  (Cambridge) in Accounts and taxation 
+            I have completed Bsc  (Cambridge) in Medical Lab Technologies. 
             
             <br />
             <br />
-            Apart from audit and taxation, some other activities that I love to do!
+            Apart from billing and lab TECHNICIAN, some other activities that I love to do!
           </p>
           <ul>
             <li className="about-activity">
@@ -34,7 +34,7 @@ function AboutCard() {
           <p style={{ color: "rgb(155 126 172)" }}>
             "Strive to build things that make a difference!"{" "}
           </p>
-          <footer className="blockquote-footer">USAMA</footer>
+          <footer className="blockquote-footer">Muazam</footer>
         </blockquote>
       </Card.Body>
     </Card>
