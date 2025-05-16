70033e63fb698d61d00f4494d0165bbfc1734a41
AZEEMAZAM1 <MUHAMMADAZEEM3010@GMAIL.COM>
2025-05-16 02:42:07 +0100

    Your commit message

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -8,24 +8,29 @@ import Type from "./Type";
 function Home() {
   return (
     <section>
-      <Container fluid className="home-section" id="home">
+      <Container
+        fluid
+        className="home-section"
+        id="home"
+        style={{ backgroundColor: "black", color: "white" }} // Black background and white text
+      >
         <Particle />
         <Container className="home-content">
           <Row>
             <Col md={7} className="home-header">
-              <h1 style={{ paddingBottom: 15 }} className="heading">
+              <h1 style={{ paddingBottom: 15, color: "white" }} className="heading">
                 Hi There!{" "}
                 <span className="wave" role="img" aria-labelledby="wave">
                   👋🏻
                 </span>
               </h1>
 
-              <h1 className="heading-name">
+              <h1 className="heading-name" style={{ color: "white" }}>
                 I'M
-                <strong className="main-name"color="white"> MUAZAM AZAM</strong>
+                <strong className="main-name"> MUAZAM AZAM</strong>
               </h1>
 
-              <div style={{ padding: 50, textAlign: "left" }}>
+              <div style={{ padding: 50, textAlign: "left", color: "white" }}>
                 <Type />
               </div>
             </Col>
@@ -35,7 +40,7 @@ function Home() {
                 src={homeLogo}
                 alt="home pic"
                 className="img-fluid"
-                style={{ maxHeight: "450px" }}
+                style={{ maxHeight: "450px", filter: "grayscale(100%)" }} // Grayscale image
               />
             </Col>
           </Row>
