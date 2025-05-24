be5fc71528bbfccd367a480fecab3d8f3c2e6410
AZEEMAZAM1 <MUHAMMADAZEEM3010@GMAIL.COM>
2025-05-24 12:55:11 +0100

    Update: added new features

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -37,7 +37,7 @@ function Projects() {
               imgPath={bitsOfCode}
               isBlog={false}
               title="Bits-0f-C0de"
-              description="My personal blog page build with Next.js and Tailwind Css which takes the content from makdown files and renders it using Next.js. Supports dark mode and easy to write blogs using markdown."
+              description="My personal blog page build with Next.js and Tailwind Css which takes the content from makdown files and renders it using Next.js. and i use this to mange certain foums and to elaborate my knowldege using chatbot agents."
               // ghLink="https://github.com/soumyajit4419/Bits-0f-C0de"
               // demoLink="https://blogs.soumya-jit.tech/"
             />
